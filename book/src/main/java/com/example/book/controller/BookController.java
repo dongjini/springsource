034c85276f242fe0187cd17e577e4848afafa6e2
joung dong jin <jdj5201@gmail.com>
2025-04-24 18:03:43 +0900

    spring book 예제

---
+++ book/src/main/java/com/example/book/controller/BookController.java
@@ -0,0 +1,39 @@
+package com.example.book.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.book.dto.BookDTO;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Log4j2
+@Controller
+@RequestMapping("/book")
+public class BookController {
+
+    @GetMapping("/list")
+    public void getList() {
+        log.info("book list 요청");
+    }
+
+    @GetMapping({ "/read", "/modify" })
+    public void getRead(Long code) {
+        log.info("book get 요청 {}", code);
+    }
+
+    @PostMapping("/modify")
+    public void postModify(BookDTO dto) {
+        log.info("book modity 요청 {}", dto);
+
+    }
+
+    @PostMapping("/remove")
+    public void postRemove(Long code) {
+        log.info("book remove 요청 {}", code);
+
+    }
+
+}
---
+++ book/src/main/java/com/example/book/dto/BookDTO.java
@@ -0,0 +1,25 @@
+package com.example.book.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BookDTO {
+    private Long code;
+    private String title;
+    private String author;
+    private int price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+}
