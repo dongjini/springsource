2bc0c37daa2f10ffd6d2efeb1bb5683ae06ac37e
joung dong jin <jdj5201@gmail.com>
2025-04-29 18:07:19 +0900

    spring board 예제

---
+++ board/src/main/java/com/example/board/entity/Member.java
@@ -0,0 +1,30 @@
+package com.example.board.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@ToString
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "BOARD_MEMBER")
+public class Member extends BaseEntity {
+
+    @Id
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+}
